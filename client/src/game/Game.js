03e458fcda8c5ea79603db3572feb06b42408fbe
JoscaWij <Josca.W@gmx.net>
2020-09-11 16:33:59 +0200

    Refacture gameLoop.js - Game.js

---
+++ client/src/game/Game.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Game = (props) => <canvas {...props} />;
+
+export default Game;
+
+/* export const createCanvas = () => {
+  const canvas = React.createElement("canvas", {
+    width: 375,
+    height: 812,
+  });
+  return canvas;
+}; */
