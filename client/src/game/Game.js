af119b9d88efb09f981607ed99fb7eb2463078d3
JoscaWij <Josca.W@gmx.net>
2020-09-11 17:25:00 +0200

    Add context of canvas after mount and paint canvas black

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,7 +1,14 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Game = (props) => {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  }, []);
 
   return <canvas ref={canvasRef} {...props} />;
 };
