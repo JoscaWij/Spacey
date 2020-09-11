c27d7a8b08fb7bc03510db8835b385d9ef57786a
JoscaWij <Josca.W@gmx.net>
2020-09-11 17:25:46 +0200

    Draw hotpink rectangle on canvas

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -8,6 +8,8 @@ const Game = (props) => {
     const context = canvas.getContext("2d");
     context.fillStyle = "#000000";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "#ff0167";
+    context.fillRect(30, 30, 30, 30);
   }, []);
 
   return <canvas ref={canvasRef} {...props} />;
