f2156bdc9704158ac735ccfe3f353586c606d3b5
Josca Wijtenburg <68224111+JoscaWij@users.noreply.github.com>
2020-09-11 11:29:39 +0200

    Storybook setup (#26)
    * Install storybook
    
    * Add only one example in storybook and delete others
    
    * Add TestComponent to storybook
    
    * Delete unnecessary assets in storybook

---
+++ client/src/stories/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+  return <div>This is a Test-Component</div>;
+}
+
+export default Test;
---
+++ client/src/stories/Test.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Test from "./Test";
+
+export default {
+  title: "Game/Test",
+  component: Test,
+};
+
+const Template = () => <Test />;
+
+export const TestComponent = Template.bind({});
