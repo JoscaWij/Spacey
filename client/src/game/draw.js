6e2a50dbafac9ccef1225c53f597082cae9f2e7a
JoscaWij <Josca.W@gmx.net>
2020-09-14 16:09:32 +0200

    Delete example rectangle

--- client/src/game/draw.js
+++ client/src/game/draw.js
@@ -1,6 +1,4 @@
 export default function draw(context) {
   context.fillStyle = "#000000";
   context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-  context.fillStyle = "#ff0167";
-  context.fillRect(30, 30, 30, 30);
 }
