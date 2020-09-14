70137b1eae69985e1562e1e661d74f5930f91a80
JoscaWij <Josca.W@gmx.net>
2020-09-14 11:17:32 +0200

    Outsorce drawFunction into draw.js

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,20 +1,14 @@
 import React, { useEffect, useRef } from "react";
+import draw from "./Draw";
 
 const Game = (props) => {
   const canvasRef = useRef(null);
-
-  const draw = (context) => {
-    context.fillStyle = "#000000";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    context.fillStyle = "#ff0167";
-    context.fillRect(30, 30, 30, 30);
-  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     draw(context);
-  }, [draw]);
+  }, []);
 
   return <canvas ref={canvasRef} {...props} />;
 };
