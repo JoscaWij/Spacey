11c931ea584209a496bea8b12d1087b8fc98c9b1
JoscaWij <Josca.W@gmx.net>
2020-09-14 11:35:02 +0200

    Add resize function

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import draw from "./Draw";
 
+function resizeCanvas(canvas) {
+  canvas.width = 5000;
+  canvas.height = 5000;
+}
+
 const Game = (props) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+    resizeCanvas(canvas);
     draw(context);
   }, []);
 
