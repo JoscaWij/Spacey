bb66f8b1e7a9b073639cec75ab474f8dda645372
JoscaWij <Josca.W@gmx.net>
2020-09-17 14:20:26 +0200

    Draw 2 plattforms from object array

--- client/src/game/drawPlattforms.js
+++ client/src/game/drawPlattforms.js
@@ -1,16 +1,26 @@
 export default function drawPlattforms(context) {
-  const plattform = {
-    width: 50,
-    height: 10,
-    left: 10,
-    top: 400,
-  };
+  const plattforms = [
+    {
+      width: 50,
+      height: 10,
+      left: 10,
+      top: 400,
+    },
+    {
+      width: 50,
+      height: 10,
+      left: 100,
+      top: 400,
+    },
+  ];
 
   context.fillStyle = "#00cc66";
-  context.fillRect(
-    plattform.left,
-    plattform.top,
-    plattform.width,
-    plattform.height
+  plattforms.map((plattform) =>
+    context.fillRect(
+      plattform.left,
+      plattform.top,
+      plattform.width,
+      plattform.height
+    )
   );
 }
