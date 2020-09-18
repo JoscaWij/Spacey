f8b77935cb662e9f0a296c07815ffa18a865c880
JoscaWij <Josca.W@gmx.net>
2020-09-18 17:19:32 +0200

    Setup MenuPage

---
+++ client/src/page/MenuPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MenuPage(props) {
+  return (
+    <div>
+      <h1>Spacey</h1>
+      <img src="" alt="Astronaut sitting on a little rocket" />
+      <button>Button</button>
+    </div>
+  );
+}
+
+export default MenuPage;
