61939be42959f5f6a340b798531a5385c64b700c
Josca Wijtenburg <68224111+JoscaWij@users.noreply.github.com>
2020-09-18 15:26:05 +0200

    Add global styles (#38)
    * Setup GlobalStyles.js
    
    * Add globalStyles to App.js
    
    * Add css-variables in :root
    
    * Add html and body styling
    
    * Add background image
    
    * Add font - Roboto Mono
    
    * Add font - Permanent Marker
    
    * Delete index.css
    
    * Delete App.css with import in App.js
    
    * Add font-family to html
    
    * Add button styling
    
    * Add button hover state and adjust padding
    
    * Add background-color transparent for smooth transition to hover
    
    * Add headline styling
    
    * Replace font-family RobotoMono
    
    * Add global styles to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,14 @@
-
+import GlobalStyles from "../src/GlobalStyles";
+import React from "react";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  layout: "fullscreen",
+};
+const withGlobalStyles = (Story, context) => {
+  return (
+    <>
+      <GlobalStyles /> <Story {...context} />
+    </>
+  );
+};
+export const decorators = [withGlobalStyles];
---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+import backgroundImageSrc from "./assets/backgroundImage.png";
+import RobotoMono from "./assets/fonts/RobotoMono-VariableFont_wght.ttf";
+import PermanentMarker from "./assets/fonts/PermanentMarker-Regular.ttf";
+import asteroidImageSrc from "./assets/asteroid.png";
+
+const GlobalStyles = () => {
+  return (
+    <Global
+      styles={css`
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+        }
+
+        :root {
+          --highlight-color: #d9a491;
+          --button-gradient: linear-gradient(
+            to bottom,
+            var(--highlight-color) 0%,
+            #5a443c 80%,
+            #000000 100%
+          );
+        }
+
+        html {
+          font-size: 14px;
+          color: #fff;
+          @font-face {
+            font-family: "RobotoMono";
+            src: url(${RobotoMono}) format("truetype");
+          }
+          @font-face {
+            font-family: "PermanentMarker";
+            src: url(${PermanentMarker}) format("truetype");
+          }
+        }
+
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: "RobotoMono";
+          background-image: url(${backgroundImageSrc});
+          background-size: cover;
+          background-repeat: no-repeat;
+        }
+
+        h1,
+        h2 {
+          font-family: "PermanentMarker";
+          font-weight: lighter;
+        }
+
+        button {
+          background-image: var(--button-gradient);
+          background-color: transparent;
+          border-radius: 20px;
+          border-style: none;
+          padding: 0.5em 1.6em;
+          font-family: "RobotoMono";
+          color: #fff;
+        }
+
+        button:hover {
+          background-image: url(${asteroidImageSrc});
+          background-size: cover;
+          background-color: transparent;
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyles;
