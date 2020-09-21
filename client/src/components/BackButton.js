22002ebc9813e59731c6f6cd7d1e9cc54c0e8251
JoscaWij <Josca.W@gmx.net>
2020-09-21 18:09:38 +0200

    Add image and wrapper

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -1,7 +1,23 @@
 import React from "react";
+import gobackButtonImageSrc from "../assets/backbtnIcon.svg";
+import styled from "@emotion/styled";
 
-function BackButton(props) {
-  return <button>Button</button>;
+const InvisableButtonWrapper = styled.button`
+  padding: 0;
+  background: none;
+
+  :hover {
+    background: none;
+    box-shadow: var(--highlight-color) 0 0 10px;
+  }
+`;
+
+function BackButton() {
+  return (
+    <InvisableButtonWrapper>
+      <img src={gobackButtonImageSrc} alt="Go back button" />
+    </InvisableButtonWrapper>
+  );
 }
 
 export default BackButton;
