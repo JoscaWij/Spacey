4bc611928de923799b2024e4e5dc282e15c83f88
JoscaWij <Josca.W@gmx.net>
2020-09-21 16:49:47 +0200

    Add button to storybook

---
+++ client/src/stories/BackButton.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BackButton from "../components/BackButton";
+
+export default {
+  title: "App/BackButton",
+  component: BackButton,
+};
+
+const Template = () => <BackButton />;
+
+export const Buttontogoback = Template.bind({});
