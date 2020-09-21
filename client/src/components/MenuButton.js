cc55ce83590d299c841d6a999264e7b4db220876
JoscaWij <Josca.W@gmx.net>
2020-09-21 15:08:30 +0200

    Outsorce MenuButton

---
+++ client/src/components/MenuButton.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const MenuButton = styled.button`
+  font-size: 1.5em;
+  text-shadow: var(--highlight-color) 0 3px 5px;
+  padding: 0.7em 1em;
+  margin: 1em;
+`;
