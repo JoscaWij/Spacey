b8281068199e2ef7498e7be4faf16512c72673f0
JoscaWij <Josca.W@gmx.net>
2020-09-21 15:10:00 +0200

    Refacture MenuPage

--- client/src/page/MenuPage.js
+++ client/src/page/MenuPage.js
@@ -3,39 +3,9 @@ import styled from "@emotion/styled";
 import MenuImageSrc from "./../assets/asteronautRakete.png";
 import MenuBackgroundImageSrc from "./../assets/planet.png";
 import { Link } from "react-router-dom";
-
-const MenuWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const MenuImage = styled.img`
-  height: 45%;
-`;
-
-const MenuBackgroundImage = styled.img`
-  width: 100%;
-  height: 12%;
-  margin-top: 2em;
-`;
-
-const GameTitle = styled.h1`
-  font-size: 3rem;
-  -webkit-text-stroke: 1px #000;
-  text-shadow: var(--highlight-color) 0 3px 5px;
-`;
-
-const MenuButton = styled.button`
-  font-size: 1.5em;
-  text-shadow: var(--highlight-color) 0 3px 5px;
-  padding: 0.7em 1em;
-  margin: 1em;
-  cursor: pointer;
-`;
+import { PageWrapper } from "../components/PageWrapper";
+import { GameTitle } from "../components/GameTitle";
+import { MenuButton } from "../components/MenuButton";
 
 function MenuPage() {
   return (
@@ -54,3 +24,17 @@ function MenuPage() {
 }
 
 export default MenuPage;
+
+const MenuWrapper = styled(PageWrapper)`
+  justify-content: space-between;
+`;
+
+const MenuImage = styled.img`
+  height: 45%;
+`;
+
+const MenuBackgroundImage = styled.img`
+  width: 100%;
+  height: 12%;
+  margin-top: 2em;
+`;
