3eb5773e6dc2f0a99419d38e6f3aa03c574d289d
JoscaWij <Josca.W@gmx.net>
2020-09-21 10:26:38 +0200

    Add text shadow in highlight-color

--- client/src/page/MenuPage.js
+++ client/src/page/MenuPage.js
@@ -17,6 +17,7 @@ const MenuImage = styled.img`
 const GameTitle = styled.h1`
   font-size: 3rem;
   -webkit-text-stroke: 1px #000;
+  text-shadow: var(--highlight-color) 0 3px 5px;
 `;
 
 function MenuPage() {
