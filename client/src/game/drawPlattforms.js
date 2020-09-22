017859c5c5d1303b91cc3221cd12e97b356ae7c6
JoscaWij <Josca.W@gmx.net>
2020-09-22 11:29:13 +0200

    Outsorce plattform array and plattformheight

--- client/src/game/drawPlattforms.js
+++ client/src/game/drawPlattforms.js
@@ -1,46 +1,11 @@
-export default function drawPlattforms(context) {
-  const plattformHeight = 10;
-
-  const plattforms = [
-    {
-      width: 50,
-      left: 10,
-      top: 400,
-    },
-    {
-      width: 50,
-      left: 100,
-      top: 400,
-    },
-    {
-      width: 80,
-      left: 300,
-      top: 350,
-    },
-    {
-      width: 100,
-      left: 100,
-      top: 300,
-    },
-    {
-      width: 60,
-      left: 10,
-      top: 200,
-    },
-    {
-      width: 70,
-      left: 100,
-      top: 100,
-    },
-  ];
-
+export default function drawPlattforms(context, PLATTFOMRHEIGHT, plattforms) {
   context.fillStyle = "#00cc66";
   plattforms.map((plattform) =>
     context.fillRect(
       plattform.left,
       plattform.top,
       plattform.width,
-      plattformHeight
+      PLATTFOMRHEIGHT
     )
   );
 }
