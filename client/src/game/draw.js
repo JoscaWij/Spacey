cf3098562ed31a7825994be74a850dfa10195c70
JoscaWij <Josca.W@gmx.net>
2020-09-22 10:16:39 +0200

    Delete backgroundImage in canvas

--- client/src/game/draw.js
+++ client/src/game/draw.js
@@ -4,22 +4,6 @@ const backgroundImage = new Image();
 backgroundImage.src = backgroundImageSrc;
 
 export default function draw(context) {
-  context.beginPath();
-  context.drawImage(
-    backgroundImage,
-    0,
-    0,
-    800,
-    1280,
-    0,
-    0,
-    context.canvas.width,
-    context.canvas.height
-  );
-  context.fillStyle = "#000000";
-  context.fill();
-  context.closePath();
-
   //floor
   context.fillStyle = "#ff0167";
   context.fillRect(0, 500, context.canvas.width, 4);
