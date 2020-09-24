0d72e2e568728a4d8a7dd49cbddc5f2876a152f9
JoscaWij <Josca.W@gmx.net>
2020-09-24 10:54:25 +0200

    Remove unnecessary calcDistance function

--- client/src/game/calcDistance.js
+++
@@ -1,9 +0,0 @@
-export default function calcDistance(player, obstacle) {
-  let topDistance = player.top - obstacle.top;
-  let leftDistance = player.left - obstacle.left;
-
-  topDistance *= topDistance;
-  leftDistance *= leftDistance;
-
-  return Math.sqrt(topDistance + leftDistance);
-}
