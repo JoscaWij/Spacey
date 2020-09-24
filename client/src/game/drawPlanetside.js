256538b3c0b38ea469498d4dca1855303134370d
JoscaWij <Josca.W@gmx.net>
2020-09-24 12:09:21 +0200

    Draw planetside

--- client/src/game/drawPlanetside.js
+++ client/src/game/drawPlanetside.js
@@ -1,3 +1,24 @@
-export default function drawPlanetside() {
-  console.log("draw planet");
+import planetsideImageSrc from "./../assets/planet.png";
+
+const planetsideImage = new Image();
+planetsideImage.src = planetsideImageSrc;
+
+export default function drawPlanetside(canvas, context, floor) {
+  const planetsideTop = floor - 50;
+  const planetsideHeight = canvas.height - planetsideTop;
+  context.beginPath();
+  context.drawImage(
+    planetsideImage,
+    0,
+    0,
+    1030,
+    147,
+    0,
+    planetsideTop,
+    canvas.width,
+    planetsideHeight
+  );
+  context.fillStyle = "#000";
+  context.fill();
+  context.closePath();
 }
