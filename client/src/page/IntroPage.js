06a455c194985bcc8db05c20b8794829982d11c8
JoscaWij <Josca.W@gmx.net>
2020-09-28 17:13:20 +0200

    Add IntroTextbox to IntroPage and styling

--- client/src/page/IntroPage.js
+++ client/src/page/IntroPage.js
@@ -9,16 +9,34 @@ const IntroPageWrapper = styled(PageWrapper)`
   align-content: center;
 `;
 
+const IntroTextbox = styled.div`
+  background: transparent;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-around;
+  height: 100%;
+
+  *:first-child {
+    font-family: PermanentMarker;
+    flex-basis: 100%;
+    font-size: 1.5em;
+    font-weight: lighter;
+    text-align: center;
+  }
+`;
+
 function IntroPage() {
   return (
     <IntroPageWrapper>
       <OutlinedContainer>
-        <h4>Please help!</h4>
-        <p>
-          The little astronaut is stranded on a lonley planet. He needs to get
-          back to his spaceship as soon as possible. Can you help him to get
-          back? Its very easy. You can use the keys to walk. And the to jump.
-        </p>
+        <IntroTextbox>
+          <span>Please help!</span>
+          <p>
+            The little astronaut is stranded on a lonley planet. He needs to get
+            back to his spaceship as soon as possible. Can you help him to get
+            back? Its very easy. You can use the keys to walk. And the to jump.
+          </p>
+        </IntroTextbox>
       </OutlinedContainer>
     </IntroPageWrapper>
   );
