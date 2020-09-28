0bebe6920381fb2b8ffbed97c7fae3928ef72741
JoscaWij <Josca.W@gmx.net>
2020-09-28 14:16:29 +0200

    Refactore rotatePlayer.js

--- client/src/game/rotatePlayer.js
+++ client/src/game/rotatePlayer.js
@@ -1,3 +1,19 @@
-export default function rotatePlayer(player, direction) {
-  player.direction = direction;
+import { DIRECTIONKEYS, DIRECTIONS } from "./GameLoop";
+
+export default function rotatePlayer(player, keyState) {
+  console.log(keyState);
+  switch (keyState) {
+    case DIRECTIONKEYS.UP:
+      player.direction = DIRECTIONS.JUMPING;
+      break;
+    case DIRECTIONKEYS.RIGHT:
+      player.direction = DIRECTIONS.RIGHT;
+      break;
+    case DIRECTIONKEYS.LEFT:
+      player.direction = DIRECTIONS.LEFT;
+      break;
+    default:
+      player.direction = DIRECTIONS.FRONT;
+      break;
+  }
 }
