7068926b02ba5793a6bc6c988fa8d08d3d215494
JoscaWij <Josca.W@gmx.net>
2020-09-28 16:30:04 +0200

    Add intro text

--- client/src/page/IntroPage.js
+++ client/src/page/IntroPage.js
@@ -5,7 +5,12 @@ import { PageWrapper } from "../components/PageWrapper";
 function IntroPage() {
   return (
     <PageWrapper>
-      <OutlinedContainer>hallo</OutlinedContainer>
+      <OutlinedContainer>
+        Please help! The little astronaut is stranded on a lonley planet. He
+        needs to get back to his spaceship as soon as possible. Can you help him
+        to get back? Its very easy. You can use the keys to walk. And the to
+        jump.
+      </OutlinedContainer>
     </PageWrapper>
   );
 }
