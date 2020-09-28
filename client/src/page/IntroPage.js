6c95240090384777ed0428a20e2290b25f067ebf
JoscaWij <Josca.W@gmx.net>
2020-09-28 17:06:03 +0200

    Add text and Wrapper that centers content

--- client/src/page/IntroPage.js
+++ client/src/page/IntroPage.js
@@ -1,17 +1,26 @@
 import React from "react";
 import OutlinedContainer from "../components/OutlinedContainer";
+import styled from "@emotion/styled";
 import { PageWrapper } from "../components/PageWrapper";
+
+const IntroPageWrapper = styled(PageWrapper)`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
 
 function IntroPage() {
   return (
-    <PageWrapper>
+    <IntroPageWrapper>
       <OutlinedContainer>
-        Please help! The little astronaut is stranded on a lonley planet. He
-        needs to get back to his spaceship as soon as possible. Can you help him
-        to get back? Its very easy. You can use the keys to walk. And the to
-        jump.
+        <h4>Please help!</h4>
+        <p>
+          The little astronaut is stranded on a lonley planet. He needs to get
+          back to his spaceship as soon as possible. Can you help him to get
+          back? Its very easy. You can use the keys to walk. And the to jump.
+        </p>
       </OutlinedContainer>
-    </PageWrapper>
+    </IntroPageWrapper>
   );
 }
 
