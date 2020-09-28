43c0882a7475b896e41e3cb3b7fd2e7f0dbddfdf
JoscaWij <Josca.W@gmx.net>
2020-09-28 17:05:22 +0200

    Adjust styling of outlined box

--- client/src/components/OutlinedContainer.js
+++ client/src/components/OutlinedContainer.js
@@ -3,11 +3,16 @@ import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 
 const OutlinedDiv = styled.div`
-  border: solid var(--highlight-color) 5px;
+  border: solid var(--highlight-color) 7px;
   border-radius: 20px;
   height: 80%;
   width: 90%;
   padding: 2em;
+  background-image: linear-gradient(
+    180deg,
+    var(--highlight-color),
+    transparent
+  );
 `;
 
 function OutlinedContainer({ children }) {
