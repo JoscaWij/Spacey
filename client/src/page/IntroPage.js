9ee6122ef0c26827db4a2a698fb3645f51de8804
JoscaWij <Josca.W@gmx.net>
2020-09-28 16:27:27 +0200

    Add intropage with outlined container

---
+++ client/src/page/IntroPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import OutlinedContainer from "../components/OutlinedContainer";
+import { PageWrapper } from "../components/PageWrapper";
+
+function IntroPage() {
+  return (
+    <PageWrapper>
+      <OutlinedContainer>hallo</OutlinedContainer>
+    </PageWrapper>
+  );
+}
+
+export default IntroPage;
