e328a391e2d9b75a751ef832c834c61facf80de9
JoscaWij <Josca.W@gmx.net>
2020-09-29 12:10:47 +0200

    Add IntroPage to storybook

---
+++ client/src/stories/IntroPage.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import IntroPage from "../page/IntroPage";
+
+export default {
+  title: "Game/Intro",
+  component: IntroPage,
+};
+
+const Template = () => <IntroPage />;
+
+export const GameIntro = Template.bind({});
