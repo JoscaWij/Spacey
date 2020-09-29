abe84da50060d2959ab4c57e0bbec6df5a92c55c
JoscaWij <Josca.W@gmx.net>
2020-09-29 12:27:45 +0200

    Add image to button

--- client/src/components/ProceedButton.js
+++ client/src/components/ProceedButton.js
@@ -1,12 +1,17 @@
 import styled from "@emotion/styled";
 import React from "react";
+import proceedImageSrc from "./../assets/IconArrow_proceed.svg";
 
 const ImageButton = styled.button`
   background: none;
 `;
 
 const ProceedButton = () => {
-  return <ImageButton>weiter</ImageButton>;
+  return (
+    <ImageButton>
+      <img src={proceedImageSrc} alt="arrow right inside the proceed button" />
+    </ImageButton>
+  );
 };
 
 export default ProceedButton;
