1d57575501fd8040a27322be270004e9e6c29c7b
JoscaWij <Josca.W@gmx.net>
2020-09-30 10:35:07 +0200

    Add game camera to storybook

---
+++ client/src/stories/GameViewport.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GameViewport from "../game/GameViewport";
+
+export default {
+  title: "Game/Game",
+  component: GameViewport,
+};
+
+const Template = () => <GameViewport />;
+
+export const GameInklCamera = Template.bind({});
