daecf2afedb1bad0af88929ea8b5d535d62aeea6
JoscaWij <Josca.W@gmx.net>
2020-09-30 11:46:52 +0200

    Delete manipulation on canvas from viewport.js

--- client/src/game/GameViewport.js
+++ client/src/game/GameViewport.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Game from "./Game";
-import { calcViewportTopOffset, VIEWPORT_SIZE } from "./viewport";
+import { VIEWPORT_SIZE } from "./viewport";
 import styled from "@emotion/styled";
 
 const CameraContainer = styled.div`
   width: ${`${VIEWPORT_SIZE.width}px`};
   height: ${`${VIEWPORT_SIZE.height}px`};
   overflow-y: hidden;
-
-  canvas {
-    position: relative;
-    top: ${`${calcViewportTopOffset()}px`};
-  }
+  background: blue;
 `;
 
 const GameViewport = () => {
