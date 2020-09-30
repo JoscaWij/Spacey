5377447659891564755718fb7aff61e1e7e836f8
JoscaWij <Josca.W@gmx.net>
2020-09-30 15:14:15 +0200

    Outsorce viewport size into const and use const for styled component

--- client/src/game/GameViewport.js
+++ client/src/game/GameViewport.js
@@ -2,9 +2,14 @@ import React from "react";
 import Game from "./Game";
 import styled from "@emotion/styled";
 
+const VIEWPORT_SIZE = {
+  width: 375,
+  height: 667,
+};
+
 const CameraContainer = styled.div`
-  width: 375px;
-  height: 667px;
+  width: ${`${VIEWPORT_SIZE.width}px`};
+  height: ${`${VIEWPORT_SIZE.height}px`};
   background: transparent;
   overflow-y: hidden;
 
