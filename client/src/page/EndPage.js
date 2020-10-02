bee0f9e8860d8f4f2e5a6a6e4a81b4686787dd99
JoscaWij <Josca.W@gmx.net>
2020-10-02 15:32:09 +0200

    Add image

--- client/src/page/EndPage.js
+++ client/src/page/EndPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { PageWrapper } from "../components/PageWrapper";
+import lostInSpaceImageSrc from "../assets/playerJumping.png";
 
 const EndPageWrapper = styled(PageWrapper)`
   justify-content: center;
@@ -13,7 +14,7 @@ const EndPageWrapper = styled(PageWrapper)`
 const EndPage = () => {
   return (
     <EndPageWrapper>
-      <img src="" alt="Player illustration" />
+      <img src={lostInSpaceImageSrc} alt="Player illustration" />
       <p>Text</p>
     </EndPageWrapper>
   );
