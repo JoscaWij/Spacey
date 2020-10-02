fe5b9a6fd9e2ad8a74f19bf352448fd8c7171fd5
JoscaWij <Josca.W@gmx.net>
2020-10-02 11:47:14 +0200

    Add EndPage

---
+++ client/src/page/EndPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { PageWrapper } from "../components/PageWrapper";
+
+const EndPageWrapper = styled(PageWrapper)`
+  align-content: center;
+`;
+
+const EndPage = () => {
+  return <EndPageWrapper></EndPageWrapper>;
+};
+
+export default EndPage;
