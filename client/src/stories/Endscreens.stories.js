953311e15ddc28d0c6bcfb979ec6b4e75e37578f
JoscaWij <Josca.W@gmx.net>
2020-10-02 15:28:48 +0200

    Add endscreen to storybook

---
+++ client/src/stories/Endscreens.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EndPage from "../page/EndPage";
+
+export default {
+  title: "Game/Endscreens/Space",
+  component: EndPage,
+};
+
+const Template = () => <EndPage />;
+
+export const Endscreen = Template.bind({});
