46deab5962428802202b024fcfb7336d00591352
JoscaWij <Josca.W@gmx.net>
2020-10-02 16:42:54 +0200

    Endscreens in storybook

--- client/src/stories/Endscreens.stories.js
+++ client/src/stories/Endscreens.stories.js
@@ -1,11 +1,20 @@
 import React from "react";
 import EndPage from "../page/EndPage";
+import lostInSpaceImageSrc from "./assets/playerJumping.png";
+import finishedGameImageSrc from "./assets/asteronautRakete.png";
 
 export default {
-  title: "Game/Endscreens/Space",
+  title: "Game/Endscreens",
   component: EndPage,
 };
 
-const Template = () => <EndPage />;
+export const LostInSpaceEnding = () => (
+  <EndPage
+    imageSrc={lostInSpaceImageSrc}
+    text="Oh no! The astronaut is lost in space!"
+  />
+);
 
-export const Endscreen = Template.bind({});
+export const HappyEnding = () => (
+  <EndPage imageSrc={finishedGameImageSrc} text="YAY! The player is safe" />
+);
