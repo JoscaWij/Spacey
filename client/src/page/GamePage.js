504b6dbda1a8abf088b1eb084dec7d5a960b0069
JoscaWij <Josca.W@gmx.net>
2021-05-18 15:08:01 +0200

    Render levels according to levelnumber

--- client/src/page/GamePage.js
+++ client/src/page/GamePage.js
@@ -6,6 +6,7 @@ import Gameviewport from "./../game/level1/GameViewport";
 import lostInSpaceImageSrc from "../assets/playerJumping.png";
 import finishedGameImageSrc from "../assets/asteronautRakete.png";
 import BackToMenuButton from "../components/BackToMenuButton";
+import { useParams } from "react-router";
 
 const GameWrapper = styled(PageWrapper)`
   justify-content: center;
@@ -15,21 +16,54 @@ function GamePage() {
   const [isPlayerLost, setIsPlayerLost] = React.useState(false);
   const [isGameFinished, setIsGameFinished] = React.useState(false);
   const [resetGame, setResetGame] = React.useState(false);
+
+  let { levelnumber } = useParams();
+  console.log(levelnumber);
+
+  function renderLevel(
+    levelnumber,
+    isGameFinished,
+    isPlayerLost,
+    resetGame,
+    setIsGameFinished,
+    setIsPlayerLost,
+    setResetGame
+  ) {
+    switch (levelnumber) {
+      case "1":
+        return (
+          <GameWrapper>
+            <BackToMenuButton />
+            <Gameviewport
+              isFinished={isGameFinished}
+              isLost={isPlayerLost}
+              gameFinished={(state) => setIsGameFinished(state)}
+              playerLost={(state) => setIsPlayerLost(state)}
+              restartGame={(state) => setResetGame(state)}
+              resetGame={resetGame}
+            />
+          </GameWrapper>
+        );
+      case "2":
+        return <div>level 2 - coming soon</div>;
+      default:
+        return <div>No level choosen. Please try again</div>;
+    }
+  }
+
   return (
     <>
-      {!isGameFinished && !isPlayerLost && (
-        <GameWrapper>
-          <BackToMenuButton />
-          <Gameviewport
-            isFinished={isGameFinished}
-            isLost={isPlayerLost}
-            gameFinished={(state) => setIsGameFinished(state)}
-            playerLost={(state) => setIsPlayerLost(state)}
-            restartGame={(state) => setResetGame(state)}
-            resetGame={resetGame}
-          />
-        </GameWrapper>
-      )}
+      {!isGameFinished &&
+        !isPlayerLost &&
+        renderLevel(
+          levelnumber,
+          isGameFinished,
+          isPlayerLost,
+          resetGame,
+          setIsGameFinished,
+          setIsPlayerLost,
+          setResetGame
+        )}
       {isGameFinished && !isPlayerLost && (
         <EndPage
           imageSrc={finishedGameImageSrc}
