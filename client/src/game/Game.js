2ead0d5ce1d31f63abda992f6e9628fb82117b0c
JoscaWij <Josca.W@gmx.net>
2021-11-21 13:42:54 +0100

    Extract levelnummer from link with useParams and console.log in Game.js

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { useParams } from "react-router";
 
 function resizeCanvas(canvas) {
   canvas.width = document.body.clientWidth;
@@ -8,9 +9,13 @@ function resizeCanvas(canvas) {
 const Game = () => {
   const canvasRef = useRef(null);
 
+  let { levelnumber } = useParams();
+
   useEffect(() => {
     const canvas = canvasRef.current;
     resizeCanvas(canvas);
+
+    console.log(levelnumber);
   });
 
   return <canvas ref={canvasRef} />;
