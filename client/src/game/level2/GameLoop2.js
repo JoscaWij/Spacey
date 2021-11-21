ad7d4336efe68d97ac10d006b0c49c65886dee72
JoscaWij <Josca.W@gmx.net>
2021-11-21 17:05:41 +0100

    Add new player object and draw player accordingly

--- client/src/game/level2/GameLoop2.js
+++ client/src/game/level2/GameLoop2.js
@@ -1,10 +1,21 @@
+import drawPlayer from "../drawPlayer";
+
 function clearCanvas(canvas, context) {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+const player = {
+  width: 200,
+  height: 200,
+  left: 50,
+  top: 50,
+};
+
 export const gameLoop2 = (canvas) => {
   const context = canvas.getContext("2d");
   clearCanvas(canvas, context);
+
+  drawPlayer(player, context, 2);
 
   requestAnimationFrame(() => gameLoop2(canvas));
 };
