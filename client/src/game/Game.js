5ef5d8753ef7a61c23650a1d2db13df5e5531275
JoscaWij <Josca.W@gmx.net>
2021-11-21 13:29:06 +0100

    Outsorce resize Canvas from Game-function in Game.js

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from "react";
 
+function resizeCanvas(canvas) {
+  canvas.width = document.body.clientWidth;
+  canvas.height = window.innerHeight;
+}
+
 const Game = () => {
   const canvasRef = useRef(null);
-
-  function resizeCanvas(canvas) {
-    canvas.width = document.body.clientWidth;
-    canvas.height = window.innerHeight;
-  }
 
   useEffect(() => {
     const canvas = canvasRef.current;
