77caf82ff492b96190dc2ced3835ca4f2adbd412
JoscaWij <Josca.W@gmx.net>
2021-11-21 12:21:52 +0100

    Use body-clientWidth instead of window.innerWidth

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -4,9 +4,16 @@ const Game = () => {
   const canvasRef = useRef(null);
 
   function resizeCanvas(canvas) {
-    canvas.width = window.innerWidth;
+    canvas.width = document.body.clientWidth;
     canvas.height = window.innerHeight;
   }
+
+  console.log(
+    document.body.clientHeight,
+    document.body.clientWidth,
+    window.innerHeight,
+    window.innerWidth
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current;
