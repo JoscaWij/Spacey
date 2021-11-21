8a29e7ba9cdccc13faaab9f9f5d549f47a5b5c0c
JoscaWij <Josca.W@gmx.net>
2021-11-21 17:56:44 +0100

    Add imageSrc and choose src according to levelnumber

--- client/src/game/drawPlayer.js
+++ client/src/game/drawPlayer.js
@@ -1,10 +1,19 @@
 import playerImageSrc from "./../assets/playerSpriteSheet.png";
+import playerImageSrc2 from "./../assets/PlayerOnRocket.png";
 
 const playerImage = new Image();
-playerImage.src = playerImageSrc;
 
-export default function drawPlayer(player, context) {
+export default function drawPlayer(player, context, levelnumber) {
   const { width, height, left, top, direction } = player;
+
+  console.log(levelnumber);
+  switch (levelnumber) {
+    case "2":
+      playerImage.src = playerImageSrc2;
+      break;
+    default:
+      playerImage.src = playerImageSrc;
+  }
 
   const srcWidth = 180;
   const srcHeight = 240;
